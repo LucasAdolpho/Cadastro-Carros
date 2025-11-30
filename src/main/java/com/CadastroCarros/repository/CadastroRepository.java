03432a0e940e20e0b96bc6cdafcd038bb00c72dc
LucasAdolpho <adolpholucas65@gmail.com>
2025-11-30 10:07:53 -0300

    Create CarroRepository for Carro entity

--- src/main/java/com/CadastroCarros/repository/CadastroRepository.java
+++
@@ -1,4 +0,0 @@
-package com.CadastroCarros.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.CadastroCarros.entity.Cadastro;
-public interface CadastroRepository extends JpaRepository<Cadastro,Long>{}
