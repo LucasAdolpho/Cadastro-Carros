aa0b1f5fbfdbb0617710bbbf6b7b06db104a5ddd
LucasAdolpho <adolpholucas65@gmail.com>
2025-11-30 10:07:06 -0300

    Update and rename Cadastro.java to Carro.java

--- src/main/java/com/CadastroCarros/entity/Carro.java
+++ src/main/java/com/CadastroCarros/entity/Carro.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
 @Entity
-public class Cadastro implements Serializable {
+public class Carro implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
