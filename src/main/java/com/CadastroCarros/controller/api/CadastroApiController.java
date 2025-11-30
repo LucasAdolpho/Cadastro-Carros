3c6212eab02a7352e23e1541a0a35932a5ebaada
LucasAdolpho <adolpholucas65@gmail.com>
2025-11-30 10:03:26 -0300

    Refactor CadastroApiController to CarroApiController

--- src/main/java/com/CadastroCarros/controller/api/CadastroApiController.java
+++ src/main/java/com/CadastroCarros/controller/api/CadastroApiController.java
@@ -1,12 +1,12 @@
 package com.CadastroCarros.controller.api;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import com.CadastroCarros.entity.Cadastro;
+import com.CadastroCarros.entity.Carro;
 import com.CadastroCarros.service.CadastroService;
 @RestController @RequestMapping("/api/cadastros")
-public class CadastroApiController{
- private final CadastroService service;
- public CadastroApiController(CadastroService service){ this.service=service; }
- @GetMapping public List<Cadastro> listar(){ return service.listar(); }
- @PostMapping public Cadastro criar(@RequestBody Cadastro c){ return service.salvar(c); }
+public class CarroApiController{
+ private final CarroService service;
+ public CarroApiController(CarroService service){ this.service=service; }
+ @GetMapping public List<Carro> listar(){ return service.listar(); }
+ @PostMapping public Carro criar(@RequestBody Carro c){ return service.salvar(c); }
 }
